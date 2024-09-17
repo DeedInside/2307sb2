b7a2f12e8e2c8f268ab58985be36f22c7e6731e5
Пауль Сергей Александрович <paul_s_a@mail.ru>
2024-09-17 12:08:08 +0700

    17.09

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using _2307sb2.Model;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -13,9 +14,77 @@ namespace _2307sb2
 {
     public partial class MainWindow : Window
     {
+        List<User> users;
         public MainWindow()
         {
             InitializeComponent();
+            users = new List<User>()
+            {
+                new User("Name 1","Email","Pass", DateTime.Now),
+                new User("Name 2","Email","Pass", DateTime.Now),
+                new User("Name 3","Email","Pass", DateTime.Now)
+            };
+            ListBoxUser.ItemsSource = users;
+
+            ComboBoxUser.ItemsSource = UserNames(users);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"{check1.IsChecked.ToString()} - {check2.IsChecked.ToString()} - {check3.IsChecked.ToString()}");
+
+        }
+
+        private void check1_Checked(object sender, RoutedEventArgs e)
+        {
+            Stack_obr.Visibility = Visibility.Visible;
+        }
+
+        private void check1_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Stack_obr.Visibility = Visibility.Collapsed;
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = (RadioButton)sender;
+            MessageBox.Show($"{rb.IsChecked.ToString()} - {rb.Content}");
+        }
+
+        private void ListBoxUser_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                ListBox ListUser = (ListBox)sender;
+
+                if (ListUser != null)
+                {
+                    if (ListUser.SelectedItem != null)
+                    {
+                        MessageBox.Show(ListUser.SelectedItem.ToString());
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("error:" + ex);
+            }
+        }
+
+        private void add_user_to_list(object sender, RoutedEventArgs e)
+        {
+            users.Add(new User("new Name", "new Emal", "new Pass", DateTime.Now));
+            ListBoxUser.Items.Refresh();
+        }
+        List<string> UserNames(List<User> users)
+        {
+            List<string> result = new List<string>();
+
+            foreach (User user in users)
+            {
+                result.Add(user.Name);
+            }
+            return result;
         }
     }
 }
---
+++ Model/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2307sb2.Model
+{
+    public class User
+    {
+        public User(string name, string email, string password, DateTime date)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+            Date = date;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime Date { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Email} - {Password} - {Date.ToString()}";
+        }
+    }
+}
